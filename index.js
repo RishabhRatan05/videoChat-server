f60910782f90777c7260f13b8ba384ea1295b7bf
Rishabh Ratan <rishabhkarondiya@gmail.com>
2024-08-07 13:57:11 +0530

    only 2

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")
+const dotenv = require("dotenv")
 const http = require("http")
 const { Server } = require("socket.io")
 const cors = require("cors")
-const roomHandler = require("./roomHandler")
+dotenv.config()
 
 const port = process.env.PORT || 5000
 
@@ -17,15 +18,47 @@ const io = new Server(server, {
 })
 
 io.on("connection", (socket) => {
-  console.log("user connected")
+  console.log(`Socket Connected`, socket.id)
+  socket.on("room:join", (data) => {
+    let { room } = data
+    io.to(room).emit("user:joined", { id: socket.id })
+    socket.join(room)
+    io.to(socket.id).emit("room:join", data)
+  })
 
-  roomHandler(socket, io)
+  socket.on("user:call", ({ to, offer }) => {
+    console.log("to", to)
+    io.to(to).emit("incomming:call", { from: socket.id, offer })
+  })
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans })
+  })
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer)
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer })
+  })
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans)
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans })
+  })
+  socket.on("disconnect-user", () => {
+    io.close()
+  })
   socket.on("disconnect", () => {
-    console.log("user disconnected")
+    console.log("user disconnected", socket.id)
   })
 })
+// io.on("connection", (socket) => {
+//   console.log("user connected")
 
-app.get("/", (req, res) => {})
+//   roomHandler(socket, io)
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected")
+//   })
+// })
 
 server.listen(port, () => {
   console.log(`serving listening on port ${port}`)
